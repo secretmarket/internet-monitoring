14313c18ad1462e4aefdaf5d27f6e18536159f5b
Fait Poms <faitpoms@gmail.com>
2021-09-29 10:55:18 -0700

    Fix tcp connection hang

--- eye/eye_bot.py
+++ eye/eye_bot.py
@@ -1,9 +1,11 @@
+import platform
+import socket
+import urllib3.connection
 import http.server
 import socketserver
 import requests
 import datetime as dt
 import atexit
-import json
 from http import HTTPStatus
 
 with open('./settings.json', 'r') as f:
@@ -15,6 +17,23 @@ WEBHOOK_URL = SETTINGS['WEBHOOK_URL']
 LAST_UPDATE = dt.datetime.now()
 IS_DOWN = False
 UP_TIME = dt.datetime.now()
+
+# patch urllib connection so that requests doesn't hang periodically
+# https://github.com/psf/requests/issues/3353#issuecomment-722772458
+platform_name = platform.system()
+orig_connect = urllib3.connection.HTTPConnection.connect
+def patch_connect(self):
+    orig_connect(self)
+    if platform_name == "Linux" or platform_name == "Windows":
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 1),
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 3),
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5),
+    elif platform_name == "Darwin":
+        TCP_KEEPALIVE = 0x10
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        self.sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, 3)
+urllib3.connection.HTTPConnection.connect = patch_connect
 
 
 def send_slack_message(text: str):
@@ -29,24 +48,28 @@ def exit_fn():
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
-        print(self.path)
-        if self.path != '/ping':
-            self.send_response(HTTPStatus.NOT_FOUND, message=None)
-            self.end_headers()
-            return
-
         global LAST_UPDATE
         global IS_DOWN
         global UP_TIME
-        if IS_DOWN:
-            IS_DOWN = False
-            UP_TIME = dt.datetime.now()
-            downtime_length = (dt.datetime.now() - LAST_UPDATE).total_seconds()
-            downtime_str = str(dt.timedelta(
-                seconds=round(downtime_length)))
-            send_slack_message(
-                f'[Internet status: UP] | total downtime: {downtime_str}')
-        LAST_UPDATE = dt.datetime.now()
+
+        if self.path == '/ping':
+            if IS_DOWN:
+                IS_DOWN = False
+                UP_TIME = dt.datetime.now()
+                downtime_length = (dt.datetime.now() - LAST_UPDATE).total_seconds()
+                downtime_str = str(dt.timedelta(
+                    seconds=round(downtime_length)))
+                send_slack_message(
+                    f'[Internet status: UP] | total downtime: {downtime_str}')
+            LAST_UPDATE = dt.datetime.now()
+        elif self.path == '/hold':
+            body = json.loads(self.rfile.read(content_len))
+            hold_time = body['time']
+            LAST_UPDATE = dt.datetime.now()
+       else:
+            self.send_response(HTTPStatus.NOT_FOUND, message=None)
+            self.end_headers()
+            return
         self.send_response(HTTPStatus.OK, message=None)
         self.end_headers()
 
@@ -84,3 +107,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
